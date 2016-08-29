db89fda495d0ce6a8fa5ff2c7338a6f2f18d6e39
Jordan Ryan Reuter <me@jreut.com>
2016-08-28 21:08:38 -0400

    Add eslint to build pipeline

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var app = express();
-var port = (process.env.PORT !== undefined) ? process.env.PORT : 3000;
+const log = require('util').debuglog('server');
+const express = require('express');
+const app = express();
+const port = (process.env.PORT !== undefined) ? process.env.PORT : 3000;
 
 app.use(express.static('dist'));
 app.listen(port, function() {
-  console.log('Listening on ' + port + '.');
+  log('Listening on ' + port + '.');
 });
