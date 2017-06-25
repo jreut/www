bc1a23d856f3e9d4125cccb666233301c8689611
Jordan Ryan Reuter <jordan.reuter@scimedsolutions.com>
2017-06-25 10:59:15 -0400

    Fix syntax error in selector

--- src/programs/index.js
+++ src/programs/index.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-  document.querySelectorAll("link[rel=import][href$='.svg'").forEach(function(link) {
+  document.querySelectorAll("link[rel=import][href$='.svg']").forEach(function(link) {
     link.addEventListener('load', function() {
       var url = new URL(link.href);
       var svg = link.import.querySelector('svg');
